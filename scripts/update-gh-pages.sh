60fb400e4e75c3596650b070260f9ffa12df45f7
Johannes Würbach <johannes.wuerbach@googlemail.com>
2013-05-13 06:53:28 -0700

    Use master instead of gh-pages.

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -1,19 +1,23 @@
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
-  #copy data we're interested in to other place
-  cp -R dist $HOME/dist
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/johanneswuerbach/johanneswuerbach.github.io.git  gh-pages > /dev/null
-  #go into diractory and copy data we're interested in to that directory
-  cd gh-pages
-  cp -Rf $HOME/dist/* .
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
-  echo -e "Done magic with dist\n"
-fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then echo "Pull request"; exit 0; fi
+if [ "$TRAVIS_BRANCH" != "source" ]; then echo "Not in source branch."; exit 0; fi
+
+REPO="johanneswuerbach/johanneswuerbach.github.io"
+TARGET_BRANCH="master"
+
+echo -e "Starting to updating master.\n"
+#copy data we're interested in to other place
+cp -R dist $HOME/dist
+#go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+#using token clone target branch
+git clone --quiet --branch=${TARGET_BRANCH} https://${GH_TOKEN}@github.com/${REPO}.git ${TARGET_BRANCH} > /dev/null
+#go into diractory and copy data we're interested in to that directory
+cd $TARGET_BRANCH
+cp -Rf $HOME/dist/* .
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to $TARGET_BRANCH"
+git push -fq origin ${TARGET_BRANCH} > /dev/null
+echo -e "Done magic with dist\n"
